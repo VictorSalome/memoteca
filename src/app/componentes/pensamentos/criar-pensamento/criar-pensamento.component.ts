3758416a32cbaca7ae90c3088a243223637d5258
VictorSalome <victorsalome41@hotmail.com>
2024-04-10 14:19:00 -0300

    feat: dados inseridos para realizar a interpolação

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  constructor() { }
+pensamento = {
+  id: '1',
+  conteudo: 'Aprendendo angular',
+  autoria: 'Dev',
+  modelo: '',
+}
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
